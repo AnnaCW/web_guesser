8d1b1ea7e80ed0c4f7c7219afa3115f15d2e3fd8
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-29 22:28:09 -0600

    I4 extension guess limiting

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,29 +1,54 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-# get '/' do
-#   "Hello, World!"
-# end
+@@counter = 5
+@@secret_number = rand(100)
 
-SECRET_NUMBER = rand(100)
+  get '/' do
+    guess = params['guess'].to_i
+    # checker = count_checker(guess)
+    message = guess_message(guess)
+    color = background_color(guess)
+    erb :index, :locals => {:number => @@secret_number, :message => message, :color => color}
+  end
 
-get '/' do
-  guess = params['guess'].to_i
-  message = guess_message(guess)
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
-end
+  def reset
+    @@secret_number = rand(100)
+    @@counter = 5
+  end
+
 
 def guess_message(guess)
-  if guess > SECRET_NUMBER + 5
-    "Way too high!"
-  elsif guess > SECRET_NUMBER
-    "Too high!"
-  elsif guess < SECRET_NUMBER - 5
-    "Way too low!"
-  elsif guess < SECRET_NUMBER
-    "Too low!"
+  return "Please enter a number" if guess.nil?
+  @@counter -= 1
+  if @@counter == 0
+    reset
+    "You lose! A new number has been generated. Enter a guess"
+  elsif guess > @@secret_number + 5
+    "Way too high! You have #{@@counter} guesses remaining"
+  elsif guess > @@secret_number
+    "Too high! You have #{@@counter} guesses remaining"
+  elsif guess < @@secret_number - 5
+    "Way too low! You have #{@@counter} guesses remaining"
+  elsif guess < @@secret_number
+    "Too low! You have #{@@counter} guesses remaining"
   else
+    reset
     "You got it right!"
-    "The SECRET NUMBER is #{SECRET_NUMBER}."
+    "The SECRET NUMBER is #{@@secret_number}. A new number has been generated."
   end
 end
+
+  def background_color(guess)
+    if guess > @@secret_number + 5
+      'red'
+    elsif guess > @@secret_number
+      'orange'
+    elsif guess < @@secret_number- 5
+      'red'
+    elsif guess < @@secret_number
+      'orange'
+    elsif guess == @@secret_number
+      'green'
+    end
+  end
