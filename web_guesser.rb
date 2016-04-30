871774ab0e23e93f9f792619b3c3ceb65df2946e
Anna Weisbrodt <aweisbro@gmail.com>
2016-04-30 15:00:40 -0600

    I4 extension cheat mode

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,24 +2,30 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 @@counter = 5
-@@secret_number = rand(100)
+@@secret_number = rand(1..100)
 
   get '/' do
     guess = params['guess'].to_i
-    # checker = count_checker(guess)
-    message = guess_message(guess)
+    cheat = params['cheat']
+    checker = cheat_checker(cheat)
+    message = guess_message(guess) if !checker
     color = background_color(guess)
-    erb :index, :locals => {:number => @@secret_number, :message => message, :color => color}
+    erb :index, :locals => {:number => @@secret_number, :checker => checker, :message => message, :color => color}
   end
 
   def reset
-    @@secret_number = rand(100)
+    @@secret_number = rand(1..100)
     @@counter = 5
   end
 
+def cheat_checker(cheat)
+  if cheat == "true"
+    "Cheat mode. The secret number is #{@@secret_number}"
+  end
+end
 
 def guess_message(guess)
-  return "Please enter a number" if guess.nil?
+  return "Please enter a number 1-100" if guess == 0
   @@counter -= 1
   if @@counter == 0
     reset
@@ -33,9 +39,9 @@ def guess_message(guess)
   elsif guess < @@secret_number
     "Too low! You have #{@@counter} guesses remaining"
   else
+    winning_number = @@secret_number
     reset
-    "You got it right!"
-    "The SECRET NUMBER is #{@@secret_number}. A new number has been generated."
+    "You got it right! The SECRET NUMBER is #{winning_number}. A new number has been generated."
   end
 end
 
